2fd5a157cabf7955d5f902e4e28fb41a1e5f1bb0
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2017-12-23 21:46:00 +0000

    moving some stuff around
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26237 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- datacentered.php
+++ datacentered.php
@@ -1,12 +1,12 @@
 <?php
-require_once __DIR__.'/../../vendor/autoload.php';
 use Workerman\Worker;
+require_once __DIR__.'/../../vendor/autoload.php';
 require_once __DIR__.'/../../vendor/workerman/workerman/Autoloader.php';
-require_once __DIR__.'/datacenterd_timer.php';
-require_once __DIR__.'/datacenterd_task_server.php';
-require_once __DIR__.'/datacenterd_ws_server.php';
-require_once __DIR__.'/datacenterd_wss_server.php';
-require_once __DIR__.'/datacenterd_globaldata_server.php';
-require_once __DIR__.'/datacenterd_channel_server.php';
+require_once __DIR__.'/Components/Timer.php';
+require_once __DIR__.'/Components/Servers/Task.php';
+require_once __DIR__.'/Components/Servers/WebSocket.php';
+require_once __DIR__.'/Components/Servers/WebSocketSecurity.php';
+require_once __DIR__.'/Components/Servers/GlobalData.php';
+require_once __DIR__.'/Components/Servers/Channel.php';
 
 Worker::runAll();
