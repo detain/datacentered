19b847e80cef4045497976304879e172c6e8e80c
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2017-12-28 05:42:27 +0000

    updates to teh code that connects to the hyperv servers to get vm lists incorperating the new vps queue handler parameter to allow it to be used in a cron like scenario but provide the api resposnes from our concurrent requests
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26292 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- Tasks/HyperV.php
+++ Tasks/HyperV.php
@@ -23,14 +23,13 @@ $browser = new Browser($loop, $connector);
 $factory = new Factory($loop, $browser);
 
 $db = $GLOBALS['tf']->db;
-$db->query("select vps_id,vps_ip,vps_name,vps_root from vps_masters left join vps_master_details using (vps_id) where vps_type=11");
+$db->query("select * from vps_masters left join vps_master_details using (vps_id) where vps_type=".get_service_define('HYPERV'));
 $rows = [];
-while ($db->next_record(MYSQL_ASSOC)) {
+while ($db->next_record(MYSQL_ASSOC))
 	$rows[] = $db->Record;
-}
 foreach ($rows as $service_master) {
 	$url = "https://{$service_master['vps_ip']}/HyperVService/HyperVService.asmx?WSDL";
-	echo "Creating Client for {$service_master['vps_name']} @ {$url}\n";
+	//echo "Creating Client for {$service_master['vps_name']} @ {$url}\n";
 	$factory->createClient($url)->then(function (Client $client) use ($service_master) {
 		$api = new Proxy($client);
 		$api->GetVMList(['hyperVAdmin' => 'Administrator', 'adminPassword' => $service_master['vps_root']])->then(
@@ -38,12 +37,10 @@ foreach ($rows as $service_master) {
 				if (isset($result->GetVMListResult->VMList) && isset($result->GetVMListResult->VMList->VirtualMachineSummary)) {
 					if (isset($result->GetVMListResult->VMList->VirtualMachineSummary->VmId))
 						$result->GetVMListResult->VMList->VirtualMachineSummary = [0 => $result->GetVMListResult->VMList->VirtualMachineSummary];
-					echo $service_master['vps_name'].' SUCCESS!'.PHP_EOL;
-					$result = $result->GetVMListResult->VMList->VirtualMachineSummary;
+					vps_queue_handler($service_master, 'serverlist', $result);
 				} else {
 					echo $service_master['vps_name'].' ERROR: Command Completed but missing expected fields!'.PHP_EOL;
 				}
-				var_dump($result);
 			},
 			function (Exception $e) use ($service_master) {
 				echo $service_master['vps_name'].' ERROR: ' . $e->getMessage() . PHP_EOL;
