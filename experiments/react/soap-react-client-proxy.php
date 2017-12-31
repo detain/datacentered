69649c0d20c8bf7ce7af6788a887bfd72c33711b
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2017-12-31 06:17:20 +0000

    updating
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26310 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- experiments/react/soap-react-client-proxy.php
+++ experiments/react/soap-react-client-proxy.php
@@ -25,9 +25,22 @@ $factory = new Factory($loop, $browser);
 $db = $GLOBALS['tf']->db;
 $db->query("select * from vps_masters left join vps_master_details using (vps_id) where vps_type=".get_service_define('HYPERV'));
 $rows = [];
+$sids = [];
+while ($db->next_record(MYSQL_ASSOC)) {
+	$db->Record['newvps'] = [];
+	$db->Record['queue'] = [];
+	$rows[$db->Record['vps_id']] = $db->Record;
+	$sids[] = $db->Record['vps_id'];
+}
+//	vps_queue_handler($server_info, 'getnewvps');
+$db->query("select * from vps where vps_status='pending-setup' and vps_server in (".implode(',', $sids).")", __LINE__, __FILE__);
 while ($db->next_record(MYSQL_ASSOC))
-	$rows[] = $db->Record;
-foreach ($rows as $service_master) {
+	$rows[$db->Record['vps_server']]['newvps'][] = $db->Record;
+//	vps_queue_handler($server_info, 'getqueue');
+$db->query("select vps.*, hl1.* from vps, queue_log as hl1 left join queue_log as hl2 on hl2.history_type=hl1.history_id and hl2.history_section='vpsqueuedone' where hl1.history_section='vpsqueue' and hl1.history_type=vps_id and hl2.history_id is null and vps_server in (".implode(',', $sids).")", __LINE__, __FILE__);
+while ($db->next_record(MYSQL_ASSOC))
+	$rows[$db->Record['vps_server']]['queue'][] = $db->Record;
+foreach ($rows as $service_id => $service_master) {
 	$url = "https://{$service_master['vps_ip']}/HyperVService/HyperVService.asmx?WSDL";
 	//echo "Creating Client for {$service_master['vps_name']} @ {$url}\n";
 	$factory->createClient($url)->then(function (Client $client) use ($service_master) {
