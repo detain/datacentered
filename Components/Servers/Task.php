03a4abc3a3128153b1dfda5f9c1f6a6d56f817ff
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2017-12-31 10:00:36 +0000

    some more cutbacks on logging
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26317 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- Components/Servers/Task.php
+++ Components/Servers/Task.php
@@ -11,14 +11,14 @@ $task_worker->onWorkerStart = function($worker) {
 };
 $task_worker->onMessage = function($connection, $task_data) {
 	$task_data = json_decode($task_data, true);			// Suppose you send json data
-	echo "Starting Task {$task_data['function']}\n";
+	//echo "Starting Task {$task_data['function']}\n";
 	if (isset($task_data['function'])) {				// According to task_data to deal with the corresponding task logic
 		if (in_array($task_data['function'], ['sync_hyperv_queue', 'async_hyperv_get_list'])) {
 			require_once __DIR__.'/../../Tasks/'.$task_data['function'].'.php';
 			$return = isset($task_data['args']) ? call_user_func($task_data['function'], $task_data['args']) : call_user_func($task_data['function']);
 		}
 	}
-	echo "Ending Task {$task_data['function']}\n";
+	//echo "Ending Task {$task_data['function']}\n";
 	$connection->send(json_encode($return));			// send the result
 };
 return $task_worker;
