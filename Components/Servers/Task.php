44b42ee4b59fe87ba3d07d360dd94024da37a8ed
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2017-12-31 06:16:58 +0000

    minor updates mostly formatting
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26309 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- Components/Servers/Task.php
+++ Components/Servers/Task.php
@@ -7,13 +7,13 @@ function update_vps_list($args) {
 	return $return;
 }
 
-$task_worker = new Worker('Text://127.0.0.1:12345'); // task worker, using the Text protocol
-$task_worker->count = 100; // number of task processes can be opened more than needed
+$task_worker = new Worker('Text://127.0.0.1:12345');	// task worker, using the Text protocol
+$task_worker->count = 100; 								// number of task processes can be opened more than needed
 $task_worker->name = 'TaskWorker';
 $task_worker->onMessage = function($connection, $task_data) {
-	$task_data = json_decode($task_data, true); // Suppose you send json data
-	if (isset($task_data['function'])) // According to task_data to deal with the corresponding task logic
+	$task_data = json_decode($task_data, true);			// Suppose you send json data
+	if (isset($task_data['function']))					// According to task_data to deal with the corresponding task logic
 		$return = isset($task_data['args']) ? call_user_func($task_data['function'], $task_data['args']) : call_user_func($task_data['function']);
 
-	$connection->send(json_encode($return)); // send the result
+	$connection->send(json_encode($return));			// send the result
 };
--- datacentered.php
+++ datacentered.php
@@ -3,10 +3,10 @@ use Workerman\Worker;
 require_once __DIR__.'/../../vendor/autoload.php';
 require_once __DIR__.'/../../vendor/workerman/workerman/Autoloader.php';
 //require_once __DIR__.'/Components/Timer.php';
-require_once __DIR__.'/Components/Servers/GlobalData.php'; // GlobalData is used to share variables between processes.
-require_once __DIR__.'/Components/Servers/Channel.php'; // Channel is a distributed communication component used to complete inter-process communication or
-														// server-to-server communication based on the subscription release model with non-blocking IO
-require_once __DIR__.'/Components/Servers/WebSocketSecurity.php'; // Provides the wss:// SSL Websocket server
-require_once __DIR__.'/Components/Servers/Task.php';
-//require_once __DIR__.'/Components/Servers/WebSocket.php'; // Provides teh ws:// Websocket Server
+require_once __DIR__.'/Components/Servers/Task.php';				// Task Server allows you to easily pass off tasks and handle them in an async manor using JSON
+require_once __DIR__.'/Components/Servers/GlobalData.php';			// GlobalData is used to share variables between processes and deal with some concurrency issues
+require_once __DIR__.'/Components/Servers/Channel.php';				// Channel is a distributed communication component used to complete inter-process communication or
+																	// server-to-server communication based on the subscription release model with non-blocking IO
+//require_once __DIR__.'/Components/Servers/WebSocket.php';			// Provides teh ws:// Websocket Server
+require_once __DIR__.'/Components/Servers/WebSocketSecurity.php';	// Provides the wss:// SSL Websocket server
 Worker::runAll();
