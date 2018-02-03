f35d59c5edb3828c9b1974fdd3f6415f64422760
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2018-02-03 08:27:21 +0000

    lots of work on the vps slices page: - adding a hidden div to house a json string w/ some basic options to easily pass options to an otherwise static document+js - added passing minimum anx max slices and utilizing that in the slider, - added passing all the various relevent data like frequency, slice cost, etc. - made the vps_slices2 function load this templte now and opulate it
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26765 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- start_chat.php
+++ start_chat.php
@@ -7,7 +7,7 @@ use Workerman\Worker;
 
 if(strpos(strtolower(PHP_OS), 'win') === 0)
 	exit("start.php not support windows, please use start_for_win.bat\n");
-if(!extension_loaded('pcntl')) // Check for expansion
+if(!extension_loaded('pcntl'))
 	exit("Please install pcntl extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 if(!extension_loaded('posix'))
 	exit("Please install posix extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
