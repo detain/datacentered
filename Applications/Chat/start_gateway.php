1446534226e3b2ae49dbb6a7f08a76a3eee1d306
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2018-02-05 07:09:20 +0000

    some work on the host server stuff getting the websocket-ssl connection back to my setup, a class to store variables and data, and switched to not running as server itself for the time being more of a client
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26808 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- Applications/Chat/start_gateway.php
+++ Applications/Chat/start_gateway.php
@@ -20,14 +20,15 @@ $gateway->transport = 'ssl';
 $gateway->count = 4; // Set the number of processes, the number of gateway process recommendations and cpu the same
 $gateway->lanIp = '127.0.0.1'; // When distributed deployment set to intranet ip (non 127.0.0.1)
 $gateway->startPort = 2300; // Internal communication start port. If $ gateway-> count = 4, the starting port is 2300. 2300 2301 2302 2303 4 ports are generally used as the internal communication port
-$gateway->pingInterval = 10; // Heartbeat interval
+$gateway->pingInterval = 60; // Heartbeat interval
+$gateway->pingNotResponseLimit = 2;
 $gateway->pingData = '{"type":"ping"}'; // heartbeat data
 $gateway->registerAddress = '127.0.0.1:1236'; // Service registration address
 //$gateway->onWorkerStart = function($worker) {};
 $gateway->onConnect = function($connection) { // When the client is connected, set the connection onWebSocketConnect, that is, when the websocket handshake callback
 	$connection->onWebSocketConnect = function($connection , $http_header) {
-		if (!preg_match('/\.interserver\.net(:[0-9]+)*/m', $_SERVER['HTTP_ORIGIN'])) // Here you can determine whether the source of the connection is legal, illegal to turn off the connection.  $_SERVER['HTTP_ORIGIN'] Identifies which site's web-initiated websocket link
-			$connection->close();
+		//if (!preg_match('/\.interserver\.net(:[0-9]+)*/m', $_SERVER['HTTP_ORIGIN'])) // Here you can determine whether the source of the connection is legal, illegal to turn off the connection.  $_SERVER['HTTP_ORIGIN'] Identifies which site's web-initiated websocket link
+			//$connection->close();
 		// onWebSocketConnect Inside $_GET $_SERVER is available  var_dump($_GET, $_SERVER);
 	};
 };
