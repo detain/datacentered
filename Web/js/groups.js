a8a01b276db434121f506ea8b50935d9e6427634
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2018-02-06 17:28:44 +0000

    committinmg some udpates to various js code
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26848 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- Web/js/groups.js
+++ Web/js/groups.js
@@ -188,22 +188,22 @@ var WhatsApp = (function ToDoView(app) { //view
 		printMessage : function (gc) {
 			if (gc.group) {
 				if (gc.type) {
-					$(".chat").append('<div class="me rightchat clearfix"><span class="chat-img pull-right"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div>');
+					$(".chat").append('<div class="me rightchat clearfix"><span class="chat-img pull-right"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div></div>');
 					//$(".chat").append('<div class="chat-bubble me"><div class="rightchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div></div>');
 					//$(".chat").append("<div class='chat-bubble me'><div class='my-mouth'></div><div class='content'>" + gc.text + "</div><div class='time'>" + gc.time + "</div></div>");
 				}else {
-					$(".chat").append('<div class="you leftchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div>');
+					$(".chat").append('<div class="you leftchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div></div>');
 					//$(".chat").append('<div class="chat-bubble you"><div class="leftchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div></div>');
 					//$(".chat").append("<div class='chat-bubble you'><div class='your-mouth'></div><h4>" + gc.name + "</h4><div class='content'>" + gc.text + "</div><div class='time'>" + gc.time + "</div></div>");
 				}
 			}
 			else {
 				if (gc.type) {
-					$(".chat").append('<div class="me rightchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div>');
+					$(".chat").append('<div class="me rightchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div></div>');
 					//$(".chat").append('<div class="chat-bubble me"><div class="rightchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div></div>');
 					//$(".chat").append("<div class='chat-bubble me'><div class='my-mouth'></div><div class='content'>" + gc.text + "</div><div class='time'>" + gc.time + "</div></div>");
 				} else {
-					$(".chat").append('<div class="you leftchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div>');
+					$(".chat").append('<div class="you leftchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div></div>');
 					//$(".chat").append('<div class="chat-bubble you"><div class="leftchat clearfix"><span class="chat-img pull-left"><img src="'+gc.img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+gc.name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+gc.time+'</small></div><p>'+gc.text+'</p></div></div>');
 					//$(".chat").append("<div class='chat-bubble you'><div class='your-mouth'></div><div class='content'>" + gc.text + "</div><div class='time'>" + gc.time + "</div></div>");
 				}
@@ -333,4 +333,161 @@ $(document).ready(function() {
 
 function login_to_server() {
 	$("#loginModal").modal('hide');
+	connect();
+	return false;
 }
+
+if (typeof console == "undefined") {    this.console = { log: function (msg) {  } };}
+// If the browser does not support websocket, will use this flash automatically simulate websocket protocol, this process is transparent to developers
+WEB_SOCKET_SWF_LOCATION = "/swf/WebSocketMain.swf";
+// Open flash websocket debug
+WEB_SOCKET_DEBUG = true;
+var ws, name, client_list={}, roomId;
+
+// Connect to the server
+function connect() {
+	ws = new ReconnectingWebSocket("wss://"+document.domain+":7272"); // create websocket
+	//ws = new WebSocket("wss://"+document.domain+":7272");
+	ws.onopen = onopen; // When the socket connection is open, enter the user name
+	ws.onmessage = onmessage; // When there is a message according to the type of message shows different information
+	ws.onclose = function() {
+		console.log("Connection is closed, timing reconnection");
+		connect();
+	};
+	ws.onerror = function() {
+		console.log("An error occurred");
+	};
+}
+
+// When there is a message according to the type of message shows different information......
+function onopen() {
+	var login_data = {
+		"type": "login",
+		"client_name": document.getElementById('email').value,
+		"password": document.getElementById('password').value,
+		"room_id": 1
+	}
+	console.log("websocket handshake successfully, send login data: "+login_data);
+	ws.send(JSON.toString(login_data));
+	if (roomId == "phptty") {
+		var term = new Terminal({
+			cols: 130,
+			rows: 40,
+			cursorBlink: false
+		});
+		ws.send('{"type":"phptty_run","content":"htop"}');
+		term.open(document.body);
+		term.on('data', function(data) {
+			var myObj = {"type":"phptty","content":data};
+			ws.send(JSON.stringify(myObj));
+		});
+	}
+}
+
+// When the server sends a message
+function onmessage(e) {
+	console.log(e.data);
+	var data = JSON.parse(e.data);
+	switch(data['type']){
+		case 'ping': // Server ping client
+			ws.send('{"type":"pong"}');
+			break;;
+		case 'login': // Log in to update the user list
+			//{"type":"login","client_id":xxx,"client_name":"xxx","client_list":"[...]","time":"xxx"}
+			say(data['client_id'], data['client_name'],  data['client_name']+' Joined the chat room', data['time']);
+			if(data['client_list']) {
+				client_list = data['client_list'];
+			} else {
+				client_list[data['client_id']] = data['client_name'];
+			}
+			flush_client_list();
+			console.log(data['client_name']+" login successful");
+			break;
+		case 'say': // speaking
+			//{"type":"say","from_client_id":xxx,"to_client_id":"all/client_id","content":"xxx","time":"xxx"}
+			say(data['from_client_id'], data['from_client_name'], data['content'], data['time']);
+			break;
+		case 'log':
+			console.log(data['content'])
+			say(0, 'Server', data['content'], data['time']);
+			break;
+		case 'phptty':
+			term.write(data['content']);
+			break;
+		case 'vmstat':
+			receiveStats(data['content']);
+			break;
+		// User exits to update user list
+		case 'logout':
+			//{"type":"logout","client_id":xxx,"time":"xxx"}
+			say(data['from_client_id'], data['from_client_name'], data['from_client_name']+' Quit', data['time']);
+			delete client_list[data['from_client_id']];
+			flush_client_list();
+	}
+}
+
+// enter your name
+function show_prompt(){
+	$('#loginModal').modal('show');
+}
+
+// submit the conversation
+function onSubmit() {
+	var input = document.getElementById("textarea");
+	var to_client_id = $("#client_list option:selected").attr("value");
+	var to_client_name = $("#client_list option:selected").text();
+	ws.send('{"type":"say","to_client_id":"'+to_client_id+'","to_client_name":"'+to_client_name+'","content":"'+input.value.replace(/"/g, '\\"').replace(/\n/g,'\\n').replace(/\r/g, '\\r')+'"}');
+	input.value = "";
+	input.focus();
+}
+
+// Refresh user list box
+function flush_client_list(){
+	var userlist_window = $("#userlist");
+	var client_list_slelect = $("#client_list");
+	userlist_window.empty();
+	client_list_slelect.empty();
+	userlist_window.append('<h4>Online Users</h4><ul>');
+	client_list_slelect.append('<option value="all" id="cli_all">Everyone</option>');
+	for(var p in client_list){
+		userlist_window.append('<li id="'+p+'" class="bounceInDown"><a href="#" class="clearfix"><img src="https://bootdey.com/img/Content/user_3.jpg" alt="" class="img-circle"><div class="friend-name"><strong>'+client_list[p]+'</strong></div><div class="last-message text-muted">Lorem ipsum dolor sit amet.</div><small class="time text-muted">Yesterday</small><small class="chat-alert text-muted"><i class="fa fa-reply"></i></small></a></li>');
+		//userlist_window.append('<li id="'+p+'">'+client_list[p]+'</li>');
+		client_list_slelect.append('<option value="'+p+'">'+client_list[p]+'</option>');
+	}
+	$("#client_list").val(select_client_id);
+	userlist_window.append('</ul>');
+}
+
+// Speaking
+function say(from_client_id, from_client_name, content, time){
+	/*
+	// Analysis of Sina microblogging picture
+	content = content.replace(/(http|https):\/\/[\w]+.sinaimg.cn[\S]+(jpg|png|gif)/gi, function(img) {
+		return "<a target='_blank' href='"+img+"'>"+"<img src='"+img+"'>"+"</a>";}
+	);
+	// resolve the url
+	content = content.replace(/(http|https):\/\/[\S]+/gi, function(url) {
+		if(url.indexOf(".sinaimg.cn/") < 0)
+			return "<a target='_blank' href='"+url+"'>"+url+"</a>";
+		else
+			return url;
+		}
+	);
+	*/
+	var img = $('.profile img').attr('src');
+	$('.chat').append('<div class="rightchat clearfix"><span class="chat-img pull-right"><img src="'+img+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+from_client_name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+time+'</small></div><p>'+content+'</p></div></div>');
+	//$("#dialog").append('<li class="left clearfix"><span class="chat-img pull-left"><img src="https://bootdey.com/img/Content/user_3.jpg?'+from_client_id+'" alt="User Avatar"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+from_client_name+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o"></i> '+time+'</small></div><p>'+content+'</p></div></li>').parseEmotion();
+	//$("#dialog").append('<div class="speech_item"><img src="http://lorempixel.com/38/38/?'+from_client_id+'" class="user_icon" /> '+from_client_name+' <br> '+time+'<div style="clear:both;"></div><p class="triangle-isosceles top">'+content+'</p> </div>').parseEmotion();
+
+}
+
+$(function() {
+	select_client_id = 'all';
+	$("#client_list").change(function(){
+		select_client_id = $("#client_list option:selected").attr("value");
+	});
+	$('.face').click(function(event){
+		event.stopPropagation();
+	});
+});
+
