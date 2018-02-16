4e27f6027da3c840c80d1051746fd274a5631aac
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2018-02-16 13:11:36 +0000

    more updates to the daemons just adding and placing some checks
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@26900 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- start_chat.php
+++ start_chat.php
@@ -1,10 +1,9 @@
 #!/usr/bin/env php
 <?php
-// run with command:  php start.php start
 
-ini_set('display_errors', 'on');
 use Workerman\Worker;
 
+ini_set('display_errors', 'on');
 if(strpos(strtolower(PHP_OS), 'win') === 0)
 	exit("start.php not support windows, please use start_for_win.bat\n");
 if(!extension_loaded('pcntl'))
@@ -12,13 +11,9 @@ if(!extension_loaded('pcntl'))
 if(!extension_loaded('posix'))
 	exit("Please install posix extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 define('GLOBAL_START', 1); // The flag is globally activated
-
 require_once __DIR__ . '/../../vendor/autoload.php';
-
-Worker::$stdoutFile = __DIR__.'/stdout.log';
-
 //require_once __DIR__ . '/vendor/autoload.php';
 foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file) // Load all Applications/*/start*.php to start all services
 	require_once $start_file;
-
+Worker::$stdoutFile = __DIR__.'/stdout.log';
 Worker::runAll(); // Run all services
