de24802f986efb62b58632c033b85dbe7539df17
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2018-03-20 18:14:42 +0000

    commited update from last night.. disabled rrd logging and started logging bw to influxdb server
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@27135 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- Tasks/bandwidth.php
+++ Tasks/bandwidth.php
@@ -1,10 +1,19 @@
 <?php
 
 function bandwidth($args) {
+	$client = new \InfluxDB\Client('68.168.221.7', 8086, 'myadmin', 'MYp4ssw0rd');
+	$database = $client->selectDB('myadmin');
+	/*
 	$dir = __DIR__.'/../../../logs/rrd/'.$args['name'];
 	if (!file_exists($dir))
 		@mkdir($dir, 0777, TRUE);
+	*/
+	$points = [];
+	$time = time();
 	foreach ($args['content'] as $ip => $data) {
+		$points[] = new \InfluxDB\Point('bandwidth_in', $data['in'], ['ip' => $ip],[],$time);
+		$points[] = new \InfluxDB\Point('bandwidth_out', $data['out'], ['ip' => $ip],[],$time);
+		/*
 		if (!file_exists($dir.'/'.$ip.'.rrd')) {
 			$rrd = new RRDCreator($dir.'/'.$ip.'.rrd', 'now', 60);
 			$rrd->addDataSource('in:ABSOLUTE:60:U:U');
@@ -23,6 +32,9 @@ function bandwidth($args) {
 		$updater = new RRDUpdater($dir.'/'.$ip.'.rrd');
 		$updater->update(['in' => $data['in'],'out' => $data['out']]);
 		//echo 'Updated '.$dir.'/'.$ip.'.rrd File'.PHP_EOL;
+		*/
 	}
+	// Points will require second precision
+	$newPoints = $database->writePoints($points, \InfluxDB\Database::PRECISION_SECONDS);
 	return true;
 }
