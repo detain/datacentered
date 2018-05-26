69c77eee5e55276a0b17eb9eacbc00910ebaacc0
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2018-05-26 04:57:28 +0000

    lots of vps cron related updates
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@27619 83d82705-c35b-46b5-ad2c-ac440bdbb46f

---
+++ Tasks/vps_update_info.php
@@ -0,0 +1,11 @@
+<?php
+
+function vps_get_list($args) {
+	require_once __DIR__.'/../../../include/functions.inc.php';
+	global $global;
+	$db = $GLOBALS['tf']->db;
+	$db->query("select * from vps_masters left join vps_master_details using (vps_id) where vps_id=".$args['id']);
+	$db->next_record(MYSQL_ASSOC);
+	function_requirements('vps_queue_handler');
+	return vps_queue_handler($db->Record, 'vpsinfo', $args['content']);
+}
