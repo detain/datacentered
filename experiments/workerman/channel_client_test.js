539e39b0607b3ce24d963b6dfbe54a648e9449ec
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2018-08-09 22:13:00 +0000

    updates
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@28382 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- experiments/workerman/channel_client_test.js
+++
@@ -1,8 +0,0 @@
-// 127.0.0.1 replaced by the actual workerman where ip
-ws = new WebSocket("ws://127.0.0.1:4236");
-ws.onmessage = function(e) {
-    alert("Received server message:" + e.data);
-};
-
-// broadcast a message
-ws.send('hello world');
--- experiments/workerman/tcp_client_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "tom" | nc 127.0.0.1:2347
