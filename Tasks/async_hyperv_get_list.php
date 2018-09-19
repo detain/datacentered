d9a6eb2e74839c0ad4c19ea9d57a1f7841384e21
detain <detain@83d82705-c35b-46b5-ad2c-ac440bdbb46f>
2018-09-19 07:30:08 +0000

    updates to teh async hyperv get list code  updating the  clue soap client to the newer version and changing the methods around for it
    git-svn-id: http://creation.interserver.net/svn/myadmin/cpaneldirect/trunk/scripts/datacentered@28848 83d82705-c35b-46b5-ad2c-ac440bdbb46f

--- Tasks/async_hyperv_get_list.php
+++ Tasks/async_hyperv_get_list.php
@@ -1,62 +1,74 @@
 <?php
-use Clue\React\Buzz\Browser;
-use Clue\React\Soap\Factory;
-use Clue\React\Soap\Proxy;
-use Clue\React\Soap\Client;
 use Workerman\Connection\AsyncTcpConnection;
 
 if (ini_get('default_socket_timeout') < 1200 && ini_get('default_socket_timeout') > 1) {
 	ini_set('default_socket_timeout', 1200);
 }
 
-function async_hyperv_get_list_server(&$factory, $service_master)
+function async_hyperv_get_list_server(\Clue\React\Buzz\Browser &$browser, $service_master)
 {
 	$url = "https://{$service_master['vps_ip']}/HyperVService/HyperVService.asmx?WSDL";
 	//echo "Creating Client for {$service_master['vps_name']} @ {$url}\n";
-	$factory->createClient($url)->then(function (Client $client) use (&$factorty, $service_master) {
-		$api = new Proxy($client);
-		//echo "Running GetVMList for {$service_master['vps_name']}\n";
-		$api->GetVMList(['hyperVAdmin' => 'Administrator', 'adminPassword' => $service_master['vps_root']])->then(
-			function ($result) use (&$factory, &$client, $service_master) {
-				global $global;
-				$var = 'vps_host_'.$service_master['vps_id'];
-				if (isset($result->GetVMListResult->Success)) {
-					$result = $result->GetVMListResult;
-				}
-				if (isset($result->Success) && $result->Success == 'true' && isset($result->VMList) && isset($result->VMList->VirtualMachineSummary)) {
-					if (isset($result->VMList->VirtualMachineSummary->VmId)) {
-						$result->VMList->VirtualMachineSummary = [0 => $result->VMList->VirtualMachineSummary];
-					}
-					//echo $service_master['vps_name'].' Successfull Get VM List'.PHP_EOL;
-					function_requirements('vps_queue_handler');
-					vps_queue_handler($service_master, 'server_list', $result);
-				} else {
-					//echo $service_master['vps_name'].' ERROR: Command Completed but missing expected fields! Output: '.json_encode($result).PHP_EOL;
-					if (isset($result->Success) && $result->Success == 'false' && $global->$var < 3) {
-						$task_connection = new AsyncTcpConnection('Text://127.0.0.1:2208');												// Asynchronous link with the remote task service
-						$task_connection->send(json_encode(['type' => 'hyperv_cleanupresources', 'args' => ['service_master' => $service_master, 'queue' => ['server_list']]]));	// send data
-						$task_connection->onMessage = function ($task_connection, $task_result) use ($task_connection) {					// get the result asynchronously
-							$task_connection->close();																					// remember to turn off the asynchronous link after getting the result
-						};
-						$task_connection->connect();																					// execute async link
-					}
-				}
-				$global->$var = 0;
-			},
-			function (Exception $e) use ($service_master) {
-				echo $service_master['vps_name'].' GetVMList ERROR: ' . $e->getMessage() . PHP_EOL;
-				global $global;
-				$var = 'vps_host_'.$service_master['vps_id'];
-				$global->$var = 0;
+	$browser->get($url)->then(
+		function (\Psr\Http\Message\ResponseInterface $response) use ($browser, $service_master) {
+			// WSDL file is ready, create client
+			try {
+				$client = new \Clue\React\Soap\Client($browser, (string)$response->getBody(), ['soap_version' => SOAP_1_2]);
+			} catch (\SoapFault $e) {
+				echo 'Error: ' . $e->getMessage() . PHP_EOL;
+				return;
 			}
-		);
+			$api = new Proxy($client);
+			//echo "Running GetVMList for {$service_master['vps_name']}\n";
+			$api->GetVMList(['hyperVAdmin' => 'Administrator', 'adminPassword' => $service_master['vps_root']])->then(
+				function ($result) use (&$factory, &$client, $service_master) {
+					global $global;
+					$var = 'vps_host_'.$service_master['vps_id'];
+					if (isset($result->GetVMListResult->Success)) {
+						$result = $result->GetVMListResult;
+					}
+					if (isset($result->Success) && $result->Success == 'true' && isset($result->VMList) && isset($result->VMList->VirtualMachineSummary)) {
+						if (isset($result->VMList->VirtualMachineSummary->VmId)) {
+							$result->VMList->VirtualMachineSummary = [0 => $result->VMList->VirtualMachineSummary];
+						}
+						//echo $service_master['vps_name'].' Successfull Get VM List'.PHP_EOL;
+						function_requirements('vps_queue_handler');
+						vps_queue_handler($service_master, 'server_list', $result);
+					} else {
+						//echo $service_master['vps_name'].' ERROR: Command Completed but missing expected fields! Output: '.json_encode($result).PHP_EOL;
+						if (isset($result->Success) && $result->Success == 'false' && $global->$var < 3) {
+							$task_connection = new AsyncTcpConnection('Text://127.0.0.1:2208');                                                // Asynchronous link with the remote task service
+							$task_connection->send(json_encode(['type' => 'hyperv_cleanupresources', 'args' => ['service_master' => $service_master, 'queue' => ['server_list']]]));    // send data
+							$task_connection->onMessage = function ($task_connection, $task_result) use ($task_connection) {                    // get the result asynchronously
+								$task_connection->close();                                                                                    // remember to turn off the asynchronous link after getting the result
+							};
+							$task_connection->connect();                                                                                    // execute async link
+						}
+					}
+					$global->$var = 0;
+				},
+				function (Exception $e) use ($service_master) {
+					echo $service_master['vps_name'].' GetVMList ERROR: ' . $e->getMessage() . PHP_EOL;
+					global $global;
+					$var = 'vps_host_'.$service_master['vps_id'];
+					$global->$var = 0;
+				}
+			);
+		},
+		function (\Exception $e) {
+			echo 'Error: an error occured while trying to download the WSDL'.PHP_EOL;
+			return;
+		}
+	);
+	$factory->createClient($url)->then(function (Client $client) use (&$factorty, $service_master) {
+		$api = new \Clue\React\Soap\Proxy($client);
 	});
 }
 
 function async_hyperv_get_list($args)
 {
 	require_once __DIR__.'/../../../include/functions.inc.php';
-	$loop = React\EventLoop\Factory::create();
+	$loop = \React\EventLoop\Factory::create();
 	$connector = new \React\Socket\Connector($loop, [
 		//'dns' => '127.0.0.1',
 		//'tcp' => [
@@ -67,8 +79,7 @@ function async_hyperv_get_list($args)
 			'verify_peer_name' => false
 		]
 	]);
-	$browser = new Browser($loop, $connector);
-	$factory = new Factory($loop, $browser);
+	$browser = new \Clue\React\Buzz\Browser($loop, $connector);
 	global $global;
 	$db = $GLOBALS['tf']->db;
 	$db->query("select * from vps_masters left join vps_master_details using (vps_id) where vps_type=".get_service_define('HYPERV'));
@@ -84,9 +95,8 @@ function async_hyperv_get_list($args)
 			$global->$var = 0;
 		}
 		if ($global->cas($var, 0, 1)) {
-			async_hyperv_get_list_server($factory, $service_master);
+			async_hyperv_get_list_server($browser, $service_master);
 		}
 	}
-	$loop->run();
 	$loop->run();
 }
