e3716b0bf6d8ca413ad39b95391b945f46930d21
Joe Huss <detain@interserver.net>
2018-10-28 02:11:04 -0400

    updates

--- Tasks/bandwidth.php
+++ Tasks/bandwidth.php
@@ -9,10 +9,12 @@ function bandwidth($args)
 	$veids = [];
 	foreach ($args['content'] as $ip => $data) {
 		if (!isset($data['vps'])) {
-			Worker::safeEcho("Missing VPS for ip {$ip}\n");
+			Worker::safeEcho("[bandwidth Task] Missing VPS for ip {$ip}\n");
 		}
+        Worker::SafeEcho("[bandwidth Task] Loaded VPS {$data['vps']} with Server {$args['uid']}\n");
 		if (!isset($veids[$data['vps']])) {
 			$veids[$data['vps']] = $worker_db->select('*')->from('vps')->where('vps_server=:vps_server and (vps_hostname=:hostname or vps_vzid=:vzid)')->bindValues(['vps_server'=>$args['uid'],'hostname'=>$data['vps'],'vzid'=>$data['vps']])->row();
+            //Worker::SafeEcho("[bandwidth Task] Loaded VPS {$data['vps']} " : ".json_encode($veids[$data['vps']]).PHP_EOL);
 		}
 		$row = $veids[$data['vps']];
 		if ($row !== false) {
@@ -26,7 +28,7 @@ function bandwidth($args)
 			]);
 		}
 	}
-    Worker::safeEcho("bandwidth built up poitns ".json_encode($points).PHP_EOL);
+    Worker::safeEcho("[bandwidth Task] built up points ".json_encode($points).PHP_EOL);
 	$newPoints = $influx_database->writePoints($points, \InfluxDB\Database::PRECISION_SECONDS);
 	return true;
 }
