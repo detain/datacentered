5fc75828a4e599a300b8d635e6b7a599abf53969
Joe Huss <detain@interserver.net>
2023-01-09 14:22:51 -0500

    updated exception handling text to include the tries/maxtries informatoin

--- Applications/Chat/Events.php
+++ Applications/Chat/Events.php
@@ -290,7 +290,7 @@ class Events
                         $updated = true;
                     } catch (\PDOException $e) {
                         //$check = 'SQLSTATE[40000]: Transaction rollback: 3101 Plugin instructed the server to rollback the current transaction.';
-                        Worker::safeEcho('Got PDO Exception #'.$e->getCode().': "'.$e->getMessage()."\"\n");
+                        Worker::safeEcho('['.$try.'/'.$maxTries.'] Got PDO Exception #'.$e->getCode().': "'.$e->getMessage()."\"\n");
                         sleep($delay);
                     }
                 }
